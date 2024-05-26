f3ffa9b4227bd47dd91b6a939d8e2f0758067f03
r0930514 <r0930514@gmail.com>
2024-05-26 15:37:23 +0800

    Reset

--- src/bank/bank-controller/bank-controller.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('bank-controller')
-export class BankControllerController {}
--- src/bank/bank.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BankControllerController } from './bank-controller/bank-controller.controller';
-import { BankServiceService } from './bank-service/bank-service.service';
-
-@Module({
-  controllers: [BankControllerController],
-  providers: [BankServiceService]
-})
-export class BankModule {}
