c78e8a982dc98d9aadebb02f06014636dbafa6db
r0930514 <r0930514@gmail.com>
2024-07-11 22:58:10 +0800

    feat: 新增 verifyEmail 路由以驗證電子郵件

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,15 @@ import { UserCreateDto } from '../users/dto/user.create.dto';
 import { UsersService } from '../users/users.service';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './stratgies/jwt-auth.guard';
 
@@ -25,12 +33,23 @@ export class AuthController {
   @UseGuards(AuthGuard('local'))
   @Post('/signin')
   signin(@Req() request: Request) {
-    return this.authService.login(request.user);
+    return this.authService.generateAccessToken(request.user);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('/verify')
+  @Get('/verifyToken')
   verify(@Req() request) {
     return request.user;
+  }
+
+  @Get('/verifyEmail')
+  async verifyEmail(@Req() request) {
+    const result = await this.usersService.findByEmail({
+      username: request.body.email,
+    });
+    if (!result) {
+      throw new NotFoundException();
+    }
+    return result;
   }
 }
