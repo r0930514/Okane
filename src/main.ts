5ebd1621678560ee3e92cba523c6e0287230e127
r0930514 <r0930514@gmail.com>
2024-07-12 17:10:02 +0800

    feat: Enable CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Users example')
     .setDescription('The users API description')
