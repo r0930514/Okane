c3f6ce23c4bc1331c31b44ce451f67ca10c4b9a4
r0930514 <r0930514@gmail.com>
2025-09-28 16:00:40 +0800

    refactor: 移除不需要的東西與調整放置的位置

--- backend/src/migrations/1751904793464-migration.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface } from 'typeorm';
-
-export class Migration1751904793464 implements MigrationInterface {
-  public async up(): Promise<void> {}
-
-  public async down(): Promise<void> {}
-}
